ee4eea159b92972e148668d6d923733d7599c1bf
Ashish Sonone <ashson07@gmail.com>
2014-01-24 19:50:31 +0530

    upto part 5 partial

---
+++ lab03/utils.h
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <errno.h>
+
+
+#define MAXLINE 1000
+#define DEBUG 1
+#define IFBUG if(DEBUG==1){
+#define ENDBUG }
+
+
+//globals
+int childpid;
+int parentpid;
+
+void printArgs(char **args){
+    int i=0;
+    printf("printing args .. :\n");
+    while((args[i] != NULL) && i < MAXLINE){
+        printf("\t%s\n", args[i]);
+        i++;
+    }
+}
+/*the tokenizer function takes a string of chars and forms tokens out of it*/
+char ** tokenize(char* input){
+	int i;
+	int doubleQuotes = 0;
+	
+	char *token = (char *)malloc(1000*sizeof(char));
+	int tokenIndex = 0;
+
+	char **tokens;
+	tokens = (char **) malloc(MAXLINE*sizeof(char**));
+ 
+	int tokenNo = 0;
+	
+	for(i =0; i < strlen(input); i++){
+		char readChar = input[i];
+		
+		if (readChar == '"'){
+			doubleQuotes = (doubleQuotes + 1) % 2;
+			if (doubleQuotes == 0){
+				token[tokenIndex] = '\0';
+				if (tokenIndex != 0){
+					tokens[tokenNo] = (char*)malloc(sizeof(token));
+					strcpy(tokens[tokenNo++], token);
+					tokenIndex = 0; 
+				}
+			}
+		}
+		else if (doubleQuotes == 1){
+			token[tokenIndex++] = readChar;
+		}
+		else if (readChar == ' ' || readChar == '\n' || readChar == '\t'){
+			token[tokenIndex] = '\0';
+			if (tokenIndex != 0){
+				tokens[tokenNo] = (char*)malloc(sizeof(token));
+				strcpy(tokens[tokenNo++], token);
+				tokenIndex = 0; 
+			}
+		}
+		else{
+			token[tokenIndex++] = readChar;
+		}
+	}
+	
+	if (doubleQuotes == 1){
+		token[tokenIndex] = '\0';
+		if (tokenIndex != 0){
+			tokens[tokenNo] = (char*)malloc(sizeof(token));
+			strcpy(tokens[tokenNo++], token);
+		}
+	}
+	
+	return tokens;
+}
+
+//change directory give the args
+//  {"cd" , path , NULL }
+int change_dir(char ** args){
+    IFBUG printf("Changing directory\n");  ENDBUG
+    int state;
+    if(args[1] == NULL){
+        state = chdir("/home/ashish");
+    }
+    else{
+        state = chdir(args[1]);
+    }
+    if(state == -1){
+        printf("Error changing directory\n");
+    }
+    return state;
+}
+
+int execute_command(char **args){
+    IFBUG printArgs(args); ENDBUG
+    int pid = fork();
+    if(pid > 0){ // parent
+        IFBUG printf("parent: new child id =%d\n",pid); ENDBUG
+        IFBUG printf("parent: waiting for child to execute command\n",pid); ENDBUG
+        wait();
+        IFBUG printf("parent: execute_commad : child with pid %d returned\n",pid); ENDBUG
+        return -1;
+    }
+    else{ // child
+        IFBUG printf("arg[0] %s\n", args[0]); ENDBUG
+        IFBUG printArgs(args); ENDBUG
+        execvp(args[0], args); 
+        printf("error in executing the command\n");
+        //error, so send signal SIGINT to main program
+        kill(parentpid, SIGINT);
+        exit(0);
+        //###  execl(x,x, "dummy1",NULL); //needs path to full executable
+    }
+}
+
+void parent_handler(int sig, siginfo_t *siginfo, void *context)
+{
+    IFBUG printf("parent %d child %d\n",parentpid,childpid);  ENDBUG
+
+   if(getpid() == parentpid){
+       if(childpid != -1 && childpid!=0){
+            IFBUG printf("killing the child%d\n",parentpid,childpid);  ENDBUG
+            kill(childpid, SIGKILL);
+       }
+   }
+}
+
+void parent(){
+    //printf ("this is the parent, with id %d\n", (int) getpid ());
+    struct sigaction act;
+ 
+    memset (&act, '\0', sizeof(act));
+ 
+    /* Use the sa_sigaction field because the handles has two additional parameters */
+    act.sa_sigaction = parent_handler;
+ 
+    /* The SA_SIGINFO flag tells sigaction() to use the sa_sigaction field, not sa_handler. */
+    act.sa_flags = SA_SIGINFO;
+ 
+    if (sigaction(SIGINT, &act, NULL) < 0) {
+        perror ("sigaction");
+    }
+}
+
+void myfree(void *v){
+    if(v!=NULL) free(v);
+}
+
+
